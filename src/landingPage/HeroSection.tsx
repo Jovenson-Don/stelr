b97fbf2ed46c9d5ff82a6a63da1199c1ee59ae3a
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-03-21 23:03:42 -0400

    hero section plus

--- src/landingPage/HeroSection.tsx
+++ src/landingPage/HeroSection.tsx
@@ -1,18 +1,34 @@
 const HeroSection = () => {
-  const styles = {
+  const styles: { [key: string]: React.CSSProperties } = {
     container: {
       display: "flex",
       justifyContent: "space-between",
       alignItems: "center",
     },
+    img: {
+      width: "100%",
+    },
+    heroText: {
+      textAlign: "center",
+    },
   };
   return (
     <div style={styles.container}>
-      <div>
+      <div style={styles.heroText}>
         <h2>Global lending and investing simplified</h2>
+        <p>
+          Connecting budding entrepreneurs with potential investors seamlessly.
+          Discover, connect, and fuel innovation with ease, all in one platform.
+        </p>
+        <button>Investor Seeking Company</button>
+        <button>Company Seeking Investor</button>
       </div>
       <div>
-        <img src="https://via.placeholder.com/150" alt="hero" />
+        <img
+          style={styles.img}
+          src="../public/artwork/lending.jpg"
+          alt="lending and investing simplified"
+        />
       </div>
     </div>
   );
