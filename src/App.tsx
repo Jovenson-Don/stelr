75afe26fdb1eb51968455f098d6a6d753f20fd16
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-03-24 12:23:57 -0400

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,16 @@ import {
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
+    <Route>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Route>
   )
 );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,97 +1,94 @@
-import logo from "../assets/images/logo.svg";
+import background from "../../public/artwork/loginSignupBackground.png";
+import { Link } from "react-router-dom";
 
 function Login() {
+  const styles: { [key: string]: React.CSSProperties } = {
+    page: {
+      minHeight: "100vh",
+      background: `url(${background})`,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    card: {
+      textAlign: "center",
+      borderRadius: "1em",
+      border: "1px solid #ccc",
+      background: "white",
+      width: "30%",
+      padding: "2em",
+    },
+    form: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      marginLeft: "1em",
+    },
+    formInput: {
+      background: "rgba(128, 128, 128, 0.1)",
+      border: "1px solid #ccc",
+      padding: "0.5em",
+      marginBottom: "1em",
+      width: "90%",
+      height: "2em",
+      borderRadius: ".5em",
+    },
+    formButton: {
+      backgroundColor: "#00A6FB",
+      color: "white",
+      borderRadius: ".5em",
+      padding: "1em",
+      border: "none",
+      cursor: "pointer",
+      fontSize: "1em",
+      margin: "1em auto",
+      width: "80%",
+      maxWidth: "20em",
+    },
+    label: {
+      textAlign: "left",
+      width: "90%",
+      marginBottom: "0.5em",
+    },
+  };
+
   return (
-    <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-white">
-        <body class="h-full">
-        ```
-      */}
-
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 ">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img className="mx-auto h-10 w-auto" src={logo} alt="Your Company" />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Log in to your account
-          </h2>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Don't have an account?{" "}
-            <a
-              href="#"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-            >
-              Create account
-            </a>
-          </p>
-        </div>
+    <div style={styles.page}>
+      <div style={styles.card}>
+        <h2>Login to Account</h2>
+        <p>Please enter you email and password to continue</p>
+        <br />
+        <form style={styles.form}>
+          <label style={styles.label} htmlFor="email">
+            Email Address:
+          </label>
+          {/* <br /> */}
+          <input
+            style={styles.formInput}
+            type="email"
+            id="email"
+            name="email"
+            placeholder="johndoe123@gmail.com"
+          />
+          <br />
+          <label style={styles.label} htmlFor="password">
+            Password:
+          </label>
+          {/* <br /> */}
+          <input
+            style={styles.formInput}
+            type="password"
+            id="password"
+            name="password"
+            placeholder="********"
+          />
+          <button style={styles.formButton}>Login</button>
+        </form>
+        <p>
+          Don't have an account? <Link to="/signup">Create Account</Link>
+        </p>
       </div>
-    </>
+    </div>
   );
 }
 
